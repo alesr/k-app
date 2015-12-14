eb5499dd9464936f1bb95938388820d72fa1defc
alesr <alessandro.resta@gmail.com>
2015-12-14 14:32:50 +0000

    added camera

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,22 @@
 angular.module('k-app.controllers', [])
 
+.factory('Camera', ['$q', function($q) {
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}])
+
 .controller('BarsCtrl', function($scope, $stateParams, $state, Bars) {
   $scope.bars = Bars.all();
   $scope.driveMeThere = function(id) {
@@ -17,6 +34,49 @@ angular.module('k-app.controllers', [])
     $state.go('tab.photo');
   };
 })
+
+.controller("PhotoCtrl", function ($scope, $cordovaCamera) {
+
+  $scope.takePhoto = function () {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+      $scope.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function (err) {
+      // An error occured. Show a message to the user
+    });
+  }
+
+    $scope.choosePhoto = function () {
+      var options = {
+        quality: 75,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+      };
+
+      $cordovaCamera.getPicture(options).then(function (imageData) {
+          $scope.imgURI = "data:image/jpeg;base64," + imageData;
+      }, function (err) {
+          // An error occured. Show a message to the user
+      });
+    }
+  })
 
 .controller('BarDetailCtrl', function($scope, $stateParams, Bars) {
   $scope.bar = Bars.get($stateParams.barId);
--- www/js/services.js
+++ www/js/services.js
@@ -29,8 +29,8 @@ angular.module('k-app.services', [])
     geolocation: {lat: 38.576247, lng: -7.907063}
   }, {
     id: 3,
-    img_path: 'img/.jpg',
-    name: 'Na rota da Évora árabe',
+    img_path: 'img/gotico.jpg',
+    name: 'Na rota da Évora gótica',
     description1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et posuere massa. Nulla facilisi. Pellentesque finibus nisi turpis, nec pretium nisi luctus ac. Morbi rutrum odio nec nisi fermentum tincidunt. Fusce convallis urna non nunc elementum maximus. Nunc dignissim, arcu a vehicula cursus, ante sapien tristique neque, in consequat lectus tellus a nisi. Aliquam erat volutpat. Vivamus ac consectetur urna. Nunc quis hendrerit ex. Sed in vehicula odio. Etiam non porttitor ligula. Phasellus tempus libero et convallis aliquam. Ut non faucibus ex. Integer finibus mollis enim, ut ornare nibh hendrerit at. Duis in blandit ipsum. Etiam sed sagittis ipsum, eu tincidunt tortor.',
     description2: 'Maecenas porta tellus ligula, vel ultrices felis auctor vitae. Nunc consequat fermentum gravida. Donec nec dapibus ex, fringilla vehicula arcu. Nunc ullamcorper, dolor eu scelerisque vestibulum, magna lorem vehicula sem, non condimentum arcu tellus vel nulla. Quisque a eleifend dolor. Proin et sodales enim. Curabitur facilisis porta maximus. Mauris pellentesque mi ante, ut iaculis dui pellentesque rhoncus. Nam sagittis semper ipsum quis mattis. Vivamus tellus nunc, vestibulum id sem non, porta lobortis mauris. Sed commodo tincidunt dignissim. Donec at lectus id neque dictum convallis. Quisque eget elit vel lorem pellentesque finibus id in lacus. Aenean id augue elementum, finibus nisl quis, ornare nisi. Donec sollicitudin ultricies lectus sit amet ultricies.',
     directions: 'Nesta página você encontrará informações sobre como percorrer e desfrutar ao máximo da rota selecionada.',
@@ -44,7 +44,7 @@ angular.module('k-app.services', [])
     directions: 'Nesta página você encontrará informações sobre como percorrer e desfrutar ao máximo da rota selecionada.',
     geolocation: {lat: 38.582247, lng: -7.907063}
   }];
-  
+
   // var bars = [{
   //   id: 0,
   //   name: 'Oficina',
